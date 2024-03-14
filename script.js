62001f3832bb54989f326c4f110734081ac99c89
Liheng Cao <20694415+lihengcao@users.noreply.github.com>
2024-03-14 15:41:23 -0400

    intermediate changes

--- script.js
+++ script.js
@@ -6,19 +6,37 @@ const ctx = canvas.getContext("2d");
 const clear_button = document.getElementById("clear_button");
 const predict_button = document.getElementById("predict_button");
 
+let display = document.getElementById("prediction-display");
+
 const sess = new onnx.InferenceSession();
 const loadingModelPromise = sess.loadModel("./onnx_model.onnx");
 
 function clear_canvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log('clear_canvas');
+    clear_display();
 }
 
+var num = 0;
 function predict_character() {
-    clear_canvas();
+    console.log('predict_character');
+    num += 1;
+    display_result(num);
 
-    // const input = 
+    const input = get_canvas_data();
 
     const ses = new onnx.InferenceSession();
 }
 
-// function 
+function clear_display() {
+    display_result('');
+}
+
+function display_result(pred) {
+    display.innerText = pred;
+}
+
+
+function get_canvas_data() {
+    return 1;
+}
