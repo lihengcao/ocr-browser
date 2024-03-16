f05ab991e84ce2d7f429ad3185e47bc6253509f7
Liheng Cao <20694415+lihengcao@users.noreply.github.com>
2024-03-15 21:49:00 -0400

    feature completion

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # About
 Simple proof of concept project to train a model and run inference on it completely in the browser.
-Used: PyTorch to train a EMNIST CNN, ONNX to run in the browser, and a canvas DOM element to get the inference input.
+Used: PyTorch to train my own EMNIST CNN, ONNX to run in the browser, and a canvas DOM element to get the inference input.
+There are definitely more accurate models out there, but for a proof of concept, this is good enough. 
 
-# Todo
-Git submodules to get the training and exporting to `.onnx` 
+# Todo/future/nice-to-have
+- Git submodules to get the training and exporting to `.onnx`
+
+# Challenges I faced
+- Getting the model to import correctly. Some features/layers aren't supported in certain ONNX versions?
+- runtime library inference output is empty
+- I initialled wanted to have a large and high-res canvas. The challenge there was that I'd have to "coerce" it in the correct size of the output. But it was easier to just have a low-res canvas get "magnified."
--- script.js
+++ script.js
@@ -1,42 +1,107 @@
+// canvas
+// a little bit of decoupling in these vars, but should be ok!
+const CANVAS_DISPLAY_MULTIPLIER = 10;
 const CANVAS_SIZE = 280;
-
+const INPUT_SIZE = 28;
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
 
+const ctx = canvas.getContext("2d", { willReadFrequently: true });
+ctx.lineWidth = 1;
+ctx.lineCap = 'round';
+ctx.strokeStyle = 'black';
+
+let isDrawing = false;
+let prevX, prevY;
+
+canvas.addEventListener('mousedown', (e) => {
+    const rect = canvas.getBoundingClientRect();
+    prevX = (e.clientX - rect.left) / CANVAS_DISPLAY_MULTIPLIER;
+    prevY = (e.clientY - rect.top) / CANVAS_DISPLAY_MULTIPLIER;
+    isDrawing = true;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+  if (!isDrawing) return;
+
+  const rect = canvas.getBoundingClientRect();
+  const mouseX = (e.clientX - rect.left) / CANVAS_DISPLAY_MULTIPLIER;
+  const mouseY = (e.clientY - rect.top) / CANVAS_DISPLAY_MULTIPLIER;
+
+  ctx.beginPath();
+  ctx.moveTo(prevX, prevY);
+  ctx.lineTo(mouseX, mouseY);
+  ctx.stroke();
+
+  prevX = mouseX;
+  prevY = mouseY;
+});
+
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseleave', () => isDrawing = false);
+// ---
+
+// html elements
 const clear_button = document.getElementById("clear_button");
 const predict_button = document.getElementById("predict_button");
+const prediction_display = document.getElementById("prediction-display");
+// ---
 
-let display = document.getElementById("prediction-display");
+// onnx runtime
+// gets rid of an annoying error about crossOriginIsolated mode. Probably don't need that many threads anyways!
+ort.env.wasm.numThreads = 1;
+const session = ort.InferenceSession.create('./onnx_model.onnx');
+// ---
 
-const sess = new onnx.InferenceSession();
-const loadingModelPromise = sess.loadModel("./onnx_model.onnx");
+async function predict_character() {
+    const input = {'input.1': get_input_data()};
+
+    session.then((session) => {
+        const results = session.run(input);
+        
+        results.then((results) => {
+            console.log(results)
+            const all_preds = results['13'].cpuData;
+            display_result(convert_results_to_prediction(all_preds));
+        });
+    })
+}
+
+const ind_to_char = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'd', 'e', 'f', 'g', 'h', 'n', 'q', 'r', 't'];
+
+function convert_results_to_prediction(arr) {
+    return ind_to_char[arg_max(arr)];
+}
+
+// assuming arr.length > 0 :smile:
+function arg_max(arr) {
+    let max = arr[0];
+    let ind_max = 0;
+
+    for (let i = 1; i < arr.length; ++i) {
+        if (arr[i] > max) {
+            ind_max = i;
+        }
+    }
+
+    return ind_max;
+}
 
 function clear_canvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    console.log('clear_canvas');
-    clear_display();
+    display_result();
 }
 
-var num = 0;
-function predict_character() {
-    console.log('predict_character');
-    num += 1;
-    display_result(num);
-
-    const input = get_canvas_data();
-
-    const ses = new onnx.InferenceSession();
+function display_result(text='') {
+    prediction_display.innerText = text;
 }
 
-function clear_display() {
-    display_result('');
-}
+function get_input_data() {
+    const data = ctx.getImageData(0, 0, INPUT_SIZE, INPUT_SIZE).data;
+    let arr = new Float32Array(INPUT_SIZE * INPUT_SIZE);
 
-function display_result(pred) {
-    display.innerText = pred;
-}
+    for (let i = 0; i < arr.length; ++i) {
+        arr[i] = data[i * 4 + 3]; // layouts is rgba. rgb vals are zero, so grab the alpha value. 
+    }
 
-
-function get_canvas_data() {
-    return 1;
+    return new ort.Tensor("float32", arr, [1, 1, 28, 28]);
 }
